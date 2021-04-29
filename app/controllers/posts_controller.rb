a3865488db26431584c236750f7f6a4200814bf4
Stealth-git <rebill2me@gmail.com>
2021-04-29 14:13:00 +0300

    Impliment posts#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,11 @@ class PostsController < ApplicationController
   def new
     @topics = Topic.all()
   end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body, :topic_id))
+    @post.save
+
+    redirect_to '/posts/' + @post.id.to_s
+  end
 end
